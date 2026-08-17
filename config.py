311c4b5ad5339c37bcce2fbc48f83ace920e1f02
Yoginand Bhokare <155873520+yogibhokare18@users.noreply.github.com>
2026-08-17 13:05:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Yogi",
-    "database": "wellness_center"
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME")
 }

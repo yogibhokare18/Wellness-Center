6fde7473d1934707505e74300e320b7f4d25b720
Yoginand Bhokare <155873520+yogibhokare18@users.noreply.github.com>
2026-08-17 13:52:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
+    "port": int(os.getenv("DB_PORT", "3306")),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME")
